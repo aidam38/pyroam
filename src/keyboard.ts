8f7067adb6036cca1f70911ba18d975c360113ec
Vladyslav Sitalo <sitalov@amazon.com>
2021-06-16 16:43:52 -0700

    Add keyboard mapping to remove binding to speed-up the reload

--- src/keyboard.ts
+++ src/keyboard.ts
@@ -1,10 +1,13 @@
 
 import { runActiveBlockAndWriteToNext, runActiveNotebook } from "./notebook";
 
-export const handleKeyPress = async (e) => {
+export const handleKeyPress = async (e: KeyboardEvent) => {
   if (e.code === "Enter" && e.altKey === true && !e.shiftKey) {
     runActiveBlockAndWriteToNext();
   } else if (e.code === "Enter" && e.altKey === true && e.shiftKey === true) {
     runActiveNotebook();
+  } else if (e.key === "-" && e.ctrlKey === true && e.metaKey === true)  {
+    console.log("pyroam: Removing key listener")
+    document.removeEventListener("keydown", handleKeyPress)
   }
 };
